4c327ca158c7142f4297091663591ca89fb235de
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2025-07-26 20:51:04 +0600

    feat: update routes to include articles, tags, and user management
    - Refactored route imports for consistency and clarity.
    - Added routes for articles, tags, and user management to the application.
    - Ensured proper organization of route files for better maintainability.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,12 @@
 import { Application } from "express";
-import authRoutes from "./auth/auth.routes";
+import authRoutes from "./auth/auth.route";
+import articleRoutes from "./article/articles.route";
+import tagsRouters from "./tags/tags.route";
+import userRouters from "./user/user.routes";
 
 export default (app: Application): void => {
 	app.use("/auth", authRoutes);
+	app.use("/articles", articleRoutes);
+	app.use("/tags", tagsRouters);
+	app.use("/user", userRouters);
 };
